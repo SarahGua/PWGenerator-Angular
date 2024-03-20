f1bc1c6aca7c76d8fad2fc37800c62dcd2d2ec2c
SarahGuarneri <sarahguarneri97@icloud.com>
2024-03-20 22:49:21 +0100

    states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,58 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  length = 0
+  includeSymbols = false
+  includeNumbers = false
+  includeLetters = false
+  password = ''
+
+  onChangeLength(value: Event){
+    const target = event.target as HTMLInputElement
+    const parsedValue = parseInt(value)
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue
+    }
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters
+  }
+
   onButtonClick(){
-    console.log('Button was clicked')
+    const numbers = '1234567890'
+    const letters = 'abdefghijklmnopqrstuvwz'
+    const symbols = '!£$%&=@#()'
+
+    let validChars = ''
+
+    if(this.includeLetters){
+      validChars += letters
+    }
+    if(this.includeNumbers){
+      validChars += numbers
+    }
+    if(this.includeSymbols){
+      validChars += symbols
+    }
+
+    let generetedPassword = ''
+
+    for (let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length)
+      generetedPassword += validChars[index]
+    }
+    this.password = generetedPassword
+    
   }
 }
